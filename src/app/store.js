0203b91a3c5e48dbecea7a0394749091a93eedf3
KhaliqueAhmed71 <133213285+KhaliqueAhmed71@users.noreply.github.com>
2025-06-30 05:56:10 -0700

    Update store.js

--- src/app/store.js
+++ src/app/store.js
@@ -1,13 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import { Provider } from 'react-redux';
-import { store } from './app/store';
+import { configureStore } from '@reduxjs/toolkit';
+import cartReducer from '../features/cart/cartSlice';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <Provider store={store}>
-    <App />
-  </Provider>
-);
+export const store = configureStore({
+  reducer: {
+    cart: cartReducer,
+  },
+});
